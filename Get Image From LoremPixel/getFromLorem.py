f9a247bed8d7ca977ad504d075f0eb2c0d3c6e60
Erick Sanchez <esanchez@localhost.localdomain>
2017-06-09 02:41:05 -0500

    Cambio de jpg a png

--- Get Image From LoremPixel/getFromLorem.py
+++ Get Image From LoremPixel/getFromLorem.py
@@ -4,6 +4,6 @@ import requests
 response = requests.get('http://lorempixel.com/400/200', stream=True)
 response.raise_for_status()
 response.raw.decode_content = True  # Required to decompress gzip/deflate compressed responses.
-with open('img.jpg','wb') as img:
+with open('img.png','wb') as img:
     shutil.copyfileobj(response.raw, img)
 del response
